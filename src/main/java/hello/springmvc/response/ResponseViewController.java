ecd4cec61666b0bc0e0f88c385f7dedee3c64e55
sangdob <51118110+sangdob@users.noreply.github.com>
2022-03-30 23:43:10 +0900

    응답 - 정적 리소스, 뷰 템플릿 HTTP 응답 - HTTP API, 메시지 바디에 직접 입력
    어노테이션의 반복학습 시간
    
    category:스프링 MVC - 기본기능

--- src/main/java/hello/springmvc/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/response/ResponseViewController.java
@@ -2,6 +2,7 @@ package hello.springmvc.response;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,5 +14,19 @@ public class ResponseViewController {
 	public ModelAndView responseViewV1(){
 		return new ModelAndView("response/hello")
 				.addObject("data", "hello!");
+	}
+
+	@RequestMapping("/response-view-v2")
+	public String responseViewV2(Model model){
+		model.addAttribute("data", "model v2");
+		return "response/hello";
+	}
+
+	/*
+	* 요청 파라미터가 있을 경우 ViewResolver를 실행하지 않고 요청된 경로로 페이지를 mapping한다.
+	* */
+	@RequestMapping("/response/hello")
+	public void responseViewV3(Model model){
+		model.addAttribute("data", "model v3");
 	}
 }
